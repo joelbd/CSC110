87e0b02ef63064605057c674ee8bd2122aec10b0
Joel Day <dayjoel@gmail.com>
2015-07-14 20:50:30 -0700

    Redoing previous commit

--- slope.py
+++ slope.py
@@ -5,19 +5,35 @@
 # between them and displays a calculated slope
 from graphics import *
 
+def clicks():
+  p = win.getMouse()
+  x, y = p.getX(), p.getY()
+  p.draw(win)
+  label = Text(Point(x + offset, y + offset), labelText).draw(win)
+  click = Text(clickLabelLocation, clickLabelText % (x, y))
+  click1.setSize(15)
+  click1.draw(win)
+
+clicks()
+
+
 def main():
 
+  WINDOW_WIDTH = 500
+  WINDOW_HEIGHT = 500
+
   # Define window & Assets
-  win = GraphWin("Joel's Slope Delivery System", 500,500)
-  win.setCoords(0.0,0.0,100,100)
+  win = GraphWin("Joel's Slope Delivery System", WINDOW_WIDTH, WINDOW_HEIGHT)
+  win.setCoords(0.0, 0.0, 100, 100)
   win.setBackground("snow")
 
   # Make a background grid
-  for i in range(0,101,10):
-    gridh = Line(Point(0,i), Point(500,i))
+  for i in range(0, 101, 10):
+    gridh = Line(Point(0, i), Point(WINDOW_WIDTH, i))
     gridh.setFill("LightSkyBlue1")
     gridh.draw(win)
-    gridv = Line(Point(i,0), Point(i,500))
+
+    gridv = Line(Point(i, 0), Point(i, WINDOW_HEIGHT))
     gridv.setFill("LightSkyBlue1")
     gridv.draw(win)
 
@@ -27,22 +43,38 @@ def main():
   header.draw(win)
 
   # Handle first coordinates
-  p1 = win.getMouse()
-  x1, y1 = p1.getX(), p1.getY()
-  p1.draw(win)
-  label1 = Text(Point(x1 - 2, y1 - 2), "1.").draw(win)
-  click1 = Text(Point(12,20), "Point 1: (%d, %d)" % (x1,y1))
+  offset = -2
+  labelText = "1."
+  clickLabelLocation = Point(12,20)
+  clickLabelText = "Point 1: (%d, %d)"
+
+  p = win.getMouse()
+  x, y = p.getX(), p.getY()
+  p.draw(win)
+  label = Text(Point(x + offset, y + offset), labelText).draw(win)
+  click = Text(clickLabelLocation, clickLabelText % (x, y))
   click1.setSize(15)
   click1.draw(win)
 
+  p1 = p
+  x1, y1 = x, y
+
   # Handle second coordinates
-  p2 = win.getMouse()
-  x2,y2 = p2.getX(), p2.getY()
-  p2.draw(win)
-  Text(Point(x2 + 2, y2 + 2), "2.").draw(win)
-  click2 = Text(Point(88,20), "Point 2: (%d, %d)" % (x2,y2))
+  offset = +2
+  labelText = "2."
+  clickLabelLocation = Point(88,20)
+  clickLabelText = "Point 2: (%d, %d)"
+
+  p = win.getMouse()
+  x, y = p.getX(), p.getY()
+  p.draw(win)
+  Text(Point(x + offset, y + offset), labelText).draw(win)
+  click = Text(clickLabelLocation, clickLabelText % (x, y))
   click2.setSize(15)
   click2.draw(win)
+
+  p2 = p
+  x1, y1 = x, y
 
   # Draw the line
   Line(p1, p2).draw(win)
@@ -52,7 +84,7 @@ def main():
   slope = round(slope, 1)
 
   # Output slope
-  output2 = Text(Point(50,10), "The slope of the line is: %1.1f." % (slope))
+  output2 = Text(Point(50, 10), "The slope of the line is: %1.1f." % (slope))
   output2.setSize(20)
   output2.draw(win)
   header.setText("Click anywhere to close")
